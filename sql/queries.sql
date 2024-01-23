04f174ea508f0ba9542fdb89a9029c1a9e130ccd
matthew.lau <matthew.lau@credera.co.uk>
2024-01-23 08:22:20 +0000

    feat: timestamp and email unique

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,3 @@
-SELECT orders.id, orders.product_id, users.name
+SELECT orders.id, orders.product_id, users.name, users.email, users.timestamp
 FROM orders
 INNER JOIN users ON orders.user_id=users.id;
--- sql/seed.sql
+++ sql/seed.sql
@@ -1,19 +1,26 @@
 CREATE TABLE users(
   id SERIAL NOT NULL PRIMARY KEY,
-  name VARCHAR(255)
+  name VARCHAR(255),
+  email VARCHAR(255),
+  date_of_birth DATE,
+  timestamp TIMESTAMP DEFAULT current_timestamp,
+  CONSTRAINT users_email_key UNIQUE (email)
 );
 
 CREATE TABLE products(
   id SERIAL NOT NULL PRIMARY KEY,
+  timestamp TIMESTAMP DEFAULT current_timestamp,
   name VARCHAR(255)
 );
 
 CREATE TABLE orders(
     id SERIAL NOT NULL PRIMARY KEY,
+    timestamp TIMESTAMP DEFAULT current_timestamp,
     user_id INT NOT NULL references users(id),
     product_id INT NOT NULL references products(id)
 );
 
-INSERT INTO users (name) VALUES ('Paul');
-INSERT INTO products (name) VALUES ('Apply');
+INSERT INTO users (name, email, date_of_birth) VALUES ('Paul', '1@2.com', '1990-06-12');
+INSERT INTO users (name, email, date_of_birth) VALUES ('Mary', '2@2.com', '1991-07-22');
+INSERT INTO products (name) VALUES ('Apple');
 INSERT INTO orders (user_id, product_id) VALUES (1, 1);
