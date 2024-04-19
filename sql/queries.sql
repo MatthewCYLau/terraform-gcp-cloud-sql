4f62c2ab51e23258a086d65350fb0f54897f6f20
matthew.lau <matthew.lau@credera.co.uk>
2024-04-19 08:06:52 +0100

    sql not like lower

--- sql/queries.sql
+++ sql/queries.sql
@@ -3,4 +3,6 @@ FROM orders
 INNER JOIN users ON orders.user_id=users.id;
 
 SELECT users.name, users.email FROM users WHERE users.email = '1@2.com';
+SELECT users.name, users.email FROM users WHERE lower(users.name) NOT LIKE '%paul%';
+
 EXPLAIN SELECT users.name, users.email FROM users WHERE users.email = '1@2.com';
